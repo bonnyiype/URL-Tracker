24ffc1b1d4b2456c0fad8f54776201ef559fd06a
bonnyiype <bonnyiype@gmail.com>
2022-12-09 15:51:14 +0000

    Main

---
+++ index.js
@@ -0,0 +1,30 @@
+let myArray = []
+const buttonEl = document.getElementById("input-btn")
+const inputEl = document.getElementById("input-el")
+let ulEl = document.getElementById("ul-el")
+
+
+buttonEl.addEventListener("click",function() {
+    
+    myArray.push(inputEl.value)
+    inputEl.value = ""
+    renderUrls() 
+})
+
+function renderUrls() {
+
+let listItems = ""
+for (let i = 0; i < myArray.length; i++) {
+    
+    listItems += 
+    `<li>
+        <a target='_blank' href='${myArray[i]}'>
+        ${myArray[i]}
+
+        </a>
+
+    </li>`
+    
+}
+ulEl.innerHTML = listItems  
+}
